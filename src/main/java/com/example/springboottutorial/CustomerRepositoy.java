c0b385145afb8e30509836cd50d838c4e131c206
lantolus <93911073+lantolus@users.noreply.github.com>
2022-08-30 11:23:16 +0200

    Delete src directory

--- src/main/java/com/example/springboottutorial/CustomerRepositoy.java
+++
@@ -1,8 +0,0 @@
-package com.example.springboottutorial;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface CustomerRepositoy extends CrudRepository<Customer, Integer> {
-
-    Customer findCustomerById(Integer id);
-}
